92255a2023c3e833d667c610b0f364c8195ebaa5
Polarolouis <louis.lacoste@hotmail.fr>
2024-02-20 21:43:54 +0100

    greedy_vs_rl board size change

--- greedy_vs_rl.py
+++ greedy_vs_rl.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 from utils import *
 
 # Setup an arena and store the fraction of wins from the neural network versus the greedy player to plot against iterations
-gameboard_size = 6
+gameboard_size = 8
 number_games = 20      
 
 # Game
@@ -21,9 +21,12 @@ game = ConnectTwoGame(gameboard_size)
 greedy_player = GreedyConnectTwoPlayer(game)
 random_player = RandomPlayer(game=game)
 
+# Checkpoint directory
+checkpoint_dir = f"./temp/newarch/{gameboard_size}"
+
 # Iterate over the checkpoint files present in ./temp/{gameboard_size}/ to load in the neural network
 filelist = []
-for checkpoint in os.listdir(f'./temp/{gameboard_size}/'):
+for checkpoint in os.listdir(checkpoint_dir):
     if checkpoint.endswith('.pth.tar') and len(checkpoint.split('_')) == 2:
         filelist.append(checkpoint)
 
@@ -35,25 +38,28 @@ iterations = []
 for file in filelist:
     iterations.append(int(file.split('_')[1].split('.')[0]))
 
-# The data array that will be plotted
-data = np.zeros((len(filelist), 7))
-data[:,0] = iterations
+
 
 seed = 1234
 rng.seed(seed)
-
+# The data array that will be plotted
+data = np.zeros((len(filelist), 8))
+data[:,0] = iterations
+# For saving data file
+iter_max = max(iterations)
+saved_data_file = os.path.join(checkpoint_dir, f"{seed}_N{number_games}_iter{iter_max}.npy")
 # Check if a file storing data named with boardsize and the seed exists before launching the loop
-if os.path.exists(f"./temp/{gameboard_size}/{seed}_N{number_games}.npy"):
-    data = np.load(f"./temp/{gameboard_size}/{seed}_N{number_games}.npy")
+if os.path.exists(saved_data_file):
+    data = np.load(saved_data_file)
 else:
     # Iterate over the checkpoints to confront nnet vs greedy_player
     for i in range(len(filelist)):
         nnet = NNet(game)
-        nnet.load_checkpoint(folder=f"./temp/{gameboard_size}", filename=filelist[i])
+        nnet.load_checkpoint(folder=checkpoint_dir, filename=filelist[i])
         args = dotdict({'numMCTSSims': 50, 'cpuct': 1.0})
         mcts = MCTS(game, nnet, args)
         n2p = lambda x: np.argmax(mcts.getActionProb(x, temp=0))
-        
+        print(f"Iteration {i+1}/{len(filelist)} for file {filelist[i]}")
         print("Greedy vs RL")
         # Arena for Greedy player vs NNet
         arena = Arena.Arena(player1=lambda b: greedy_player.play(b), player2=n2p, game = game)
@@ -75,7 +81,7 @@ else:
         # RL player
         data[i,5] = p2wins / number_games
         data[i,6] = draws / number_games
-    np.save(f"./temp/{gameboard_size}/{seed}_N{number_games}.npy", data)
+    np.save(saved_data_file, data)
 
 
 # Plot with legends the fractions of wins for each iterations
@@ -89,7 +95,7 @@ plt.plot(data[:,0], data[:,3], linestyle="--", label='Draws')
 plt.xlabel('Iterations')
 plt.ylabel('Fraction of wins')
 # Set the y axis to be between 0 and 1
-plt.ylim(0, 1.1)
+plt.ylim(-0.1, 1.1)
 plt.legend()
 
 plt.subplot(1,2,2)
@@ -99,11 +105,11 @@ plt.plot(data[:,0], data[:,6], linestyle="--", label='Draws')
 plt.xlabel('Iterations')
 plt.ylabel('Fraction of wins')
 # Set the y axis to be between 0 and 1
-plt.ylim(0, 1.1)
+plt.ylim(-0.1, 1.1)
 plt.legend()
 plt.suptitle(f"Fraction of wins for board of size {gameboard_size}\n{number_games} games per iteration")
 
-plt.savefig(f'./temp/{gameboard_size}/fraction_wins_{gameboard_size}board.png')
+plt.savefig(os.path.join(checkpoint_dir, f"fraction_wins_{gameboard_size}board.png"))
 plt.show()
 
 # Plot for the Random vs NNet
